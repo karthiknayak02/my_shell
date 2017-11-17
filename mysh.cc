1e4f0b5238a41206c9f3d2ffff9cc8e137f20387
Karthik Nayak <nayakkarthik02@gmail.com>
2017-11-17 15:45:22 -0500

    Add files via upload

--- mysh.cc
+++ mysh.cc
@@ -13,12 +13,17 @@
 
 using namespace std;
 
+/*
+ Name: Karthik Nayak
+ Date: Nov 17 2017
+ */
+
 void change_dir(string &directoryName) {
     char cwd[256];
     if (directoryName[0] != '/') directoryName = '/' + directoryName;
     
-    if((chdir(directoryName.c_str())) < 0) cerr << "cd: " << directoryName << ": No such file or directory" << endl;
-    else cout << "Current dir: " << getcwd(cwd, sizeof(cwd)) << endl;
+    if((chdir(directoryName.c_str())) < 0)
+        cerr << "cd: " << directoryName << ": No such file or directory" << endl;
 }
 
 void list_processes(map<int, string> &processes) {
@@ -27,11 +32,12 @@ void list_processes(map<int, string> &processes) {
     if (processes.empty()) cout << "No background processes running" << endl;
     else {
         for (i = processes.begin(); i != processes.end(); i++)
-            if (waitpid(i->first, &status, WNOHANG))
-                if (processes[i->first][0] == 'r') processes[i->first] = "completed" + processes[i->first].substr(7);
+            if (waitpid(i->first, &status, WNOHANG) && processes[i->first][0] == 'r')
+                    processes[i->first] = "completed" + processes[i->first].substr(7);
         cout << "...processes running in the background..." << endl;
         cout << "Process ID   \t Status   \tName" << endl;
-        for (i = processes.begin(); i != processes.end(); i++) cout << i->first << " \t\t " << i->second << endl;
+        for (i = processes.begin(); i != processes.end(); i++)
+            cout << i->first << " \t\t " << i->second << endl;
     }
     return;
 }
@@ -72,14 +78,15 @@ void execute_cmd(vector<string> &commands, map<int, string> &processes, bool bac
 }
 
 int main() {
-    //my things
-    string prompt = "mysh$ ", inputLine = "", filename = "";
+    char cwd[256];
+    string dir = getcwd(cwd, sizeof(cwd)), prompt = "mysh$ ", inputLine = "", filename = "";
     map<string, string> variables;
 	map<int, string> processes;
 
+    dir = dir.substr(dir.rfind('/') +1);
+    if (dir.size() == 0) dir = "/";
     
-    //start
-	cout << prompt;
+	cout << dir << " " << prompt;
 	getline(cin, inputLine);
     
 	//if (inputLine == "\0") inputLine = " ";
@@ -94,7 +101,7 @@ int main() {
         
         // if empty line
         if (commands.size() == 0) {
-            cout << prompt;
+            cout << dir << " " << prompt;
             getline(cin, inputLine);
             continue;
         }
@@ -104,15 +111,16 @@ int main() {
             bool isValid = true;
             if(commands.size() != 3) cerr << "usage: set <variable> <value>" << endl;
             else if (!isalpha(commands[1][0])) isValid = false;
-            else for (int i = 0; i < commands[1].size(); i++) if (!isalnum(commands[1][i])) isValid = false;
-            
-            if (isValid) variables[commands[1]] = commands[2];
-            else cerr << "Error: Invalid variable name" << endl;
+            else {
+                for (int i = 0; i < commands[1].size(); i++) if (!isalnum(commands[1][i])) isValid = false;
+                if (isValid) variables[commands[1]] = commands[2];
+                else cerr << "Error: Invalid variable name" << endl;
+            }
         }
         
         else if (commands[0] == "show") {
             if(commands.size() != 2) cerr << "usage: show <variable>" << endl;
-            if (variables.find(commands[1]) == variables.end()) cout << commands[1] << ": undefined variable" << endl;
+            if (variables.find(commands[1]) == variables.end()) cerr << "Error: undefined variable" << endl;
             else cout << commands[1] << " : " << variables[commands[1]] << endl;
         }
         
@@ -121,18 +129,24 @@ int main() {
             else prompt = commands[1] + "$ ";
         }
         
-        else if (commands[0] == "cd"){
+        else if (commands[0] == "cd") {
             if(commands.size() != 2) cerr << "usage: cd <directoryName>" << endl;
             else change_dir(commands[1]);
+            dir = getcwd(cwd, sizeof(cwd));
+            dir = dir.substr(dir.rfind('/') +1);
+            if (dir.size() == 0) dir = "/";
         }
         
         else if (commands[0] == "listp") list_processes(processes);
         
-        else if (commands.size() != 1 && commands[commands.size()-1] == "&") execute_cmd(commands, processes, true);
+        else if (commands.size() != 1 && commands[commands.size()-1] == "&") {
+            commands.pop_back();
+            execute_cmd(commands, processes, true);
+        }
         
         else execute_cmd(commands, processes, false);
         
-		cout << prompt;
+		cout << dir << " " << prompt;
 		getline(cin, inputLine);
 	}
     
